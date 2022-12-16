906c05885ad767cdc5f9eb7267a15863b7015b2c
PabloMur <polillomurillo@gmail.com>
2022-12-16 04:22:59 -0300

    dropzone exploration

--- src/components/report/index.tsx
+++ src/components/report/index.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import css from "./styles.css";
+import { TextField } from "ui/text-field";
+import { CustomButton } from "ui/buttons";
+import Dropzone from "dropzone";
+import { useDropzone } from "react-dropzone";
 
 const ReportMaker = () => {
-  return (
-    <div className={css.root}>
-      Aca va a estar el formulario para reportar una mascota
-    </div>
-  );
+  const inputRef = useRef<HTMLDivElement>(null);
+  const otraRef = React.createRef<HTMLDivElement>();
+
+  function Mostrar() {
+    console.log("mostrar algo");
+  }
+
+  useEffect(() => {
+    console.log(inputRef.current);
+    console.log(otraRef.current);
+  }, []);
+
+  <div className={css.root}>
+    <form className={css.form}>
+      <label htmlFor="petname">
+        <p>El nombre de tu mascota</p>
+        <TextField name="petname"></TextField>
+      </label>
+      <label htmlFor="petname">
+        <p>Imagen de tu mascota</p>
+        <div ref={inputRef}>Arrastra una imagen</div>
+        <div ref={otraRef}>Test</div>
+      </label>
+      <CustomButton onClick={Mostrar}>Roportar Mascota</CustomButton>
+      <CustomButton onClick={Mostrar}>Cancelar Reporte</CustomButton>
+    </form>
+  </div>;
 };
 
 export { ReportMaker };
